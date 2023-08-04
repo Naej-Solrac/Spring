b594b7aba06f1adc371935d3d45992c66175697c
Jean Gonzales <expretapador3@gmail.com>
2023-08-04 15:54:27 -0500

    Query methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,17 @@
 package com.platzi.market.persistence.crud;
 
+import com.platzi.market.persistence.ProductoRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> { // la clase o entity  y el tipo de dato que es su clave primaria
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> { // la clase o entity  y el tipo de dato que es su clave primaria
+    //de manera nativa
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    //Utilizando query methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria); //I mayuscula
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,Boolean estado);
 }
